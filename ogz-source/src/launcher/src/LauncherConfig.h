48131760cf32c5a51778fbe4ab53be75aedebc90
root <root@gunz.pfsense.local>
2026-03-20 00:46:10 -0700

    Set launcher patch domain to public IP

--- ogz-source/src/launcher/src/LauncherConfig.h
+++ ogz-source/src/launcher/src/LauncherConfig.h
@@ -7,7 +7,7 @@ namespace LauncherConfig
 
 constexpr char ApplicationName[] = "Open GunZ Launcher";
 constexpr char LauncherFilename[] = "launcher.exe";
-constexpr char PatchDomain[] = "";
+constexpr char PatchDomain[] = "75.3.243.94";
 constexpr u16 PatchPort = 80; // 80 is the default http port.
 constexpr size_t BlockSize = 32 * 1024; // 32 KiB
 
